b64ad2e4fa8a22af76baacdb78715adfe4d58e1e
sky4star <382103956@qq.com>
2017-08-04 15:01:16 +0800

    更新技能cd解析和合并逻辑
    maxcd表示技能需要的冷却时间（根据装备，技能不同可能有变化）
    cd表示距离技能可用还需要的时间，为零表示可用。
    开始时候

--- src/model/skillstateinfo.py
+++ src/model/skillstateinfo.py
@@ -15,7 +15,7 @@ class SkillStateInfo(object):
         skill_name = delta.skill_name if delta.skill_name is not None else self.skill_name
         skill_id = delta.skill_id if delta.skill_id is not None else self.skill_id
         max_cd = delta.max_cd if delta.max_cd is not None else self.max_cd
-        cd = delta.cd if delta.cd is not None else self.cd
+        cd = delta.cd   # CD不做合并了，delta中没有就是为零，为零表示可以使用
         cost = delta.cost if delta.cost is not None else self.cost
         canuse = delta.canuse if delta.canuse is not None else self.canuse
         up = delta.up if delta.up is not None else self.up
@@ -26,7 +26,7 @@ class SkillStateInfo(object):
         skill_name = name
         skill_id = obj['ID']
         max_cd = obj['MaxCD'] if 'MaxCD' in obj else None
-        cd = obj['CD'] if 'CD' in obj else None
+        cd = obj['CD'] if 'CD' in obj else 0        # CD默认值为零
         cost = obj['Cost'] if 'Cost' in obj else None
         canuse = obj['canuse'] if 'canuse' in obj else None
         up = obj['up'] if 'up' in obj else None
