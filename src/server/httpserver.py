d40c289adffb340e49293c192da30e8b828ee042
ypeng0126 <756524138@qq.com>
2017-08-09 18:44:09 +0800

    训练用state更新 201708091844

--- src/server/httpserver.py
+++ src/server/httpserver.py
@@ -14,9 +14,9 @@ Send a POST request::
     curl -l -H "Content-type: application/json" -X GET -d '{"wldstatic":{"ID":6442537685409333250},"wldruntime":{"State":1,"tick":66.0}}' http://localhost:8780
     curl -l -H "Content-type: application/json" -X GET -d '{"wldstatic":{"ID":9999},"wldruntime":{"State":1,"tick":528.0},"waypoint":[["(56.8, 0.0, -2.8)","(54.0, 0.0, -5.1)","(53.0, 0.0, -20.0)","(37.5, 0.0, -29.5)","(31.8, 0.0, -38.7)","(14.0, 0.0, -54.5)","(-0.6, 0.0, -61.0)","(-22.1, 0.0, -47.0)","(-33.4, 0.0, -37.5)","(-41.0, 0.0, -27.0)","(-51.0, 0.0, -13.0)","(-56.3, 0.0, 0.8)"],["(56.9, 0.0, 2.6)","(54.0, 0.0, 5.0)","(54.2, 0.0, 18.4)","(43.3, 0.0, 25.7)","(36.5, 0.0, 34.0)","(26.0, 0.0, 45.2)","(0.0, 0.0, 60.8)","(-20.6, 0.0, 51.7)","(-39.9, 0.0, 30.0)","(-52.9, 0.0, 14.8)","(-56.8, 0.0, 2.6)"],["(56.9, 0.0, -0.1)","(45.1, 0.0, -2.0)","(28.8, 0.0, 0.5)","(16.9, 0.0, 1.0)","(-11.5, 0.0, 0.3)","(-17.4, 0.0, -0.2)","(-44.9, 0.0, 2.6)","(-56.5, 0.0, 1.7)"],["(54.0, 0.0, -5.1)","(53.0, 0.0, -20.0)","(37.5, 0.0, -29.5)","(31.8, 0.0, -38.7)","(14.0, 0.0, -54.5)","(-0.6, 0.0, -61.0)","(-22.1, 0.0, -47.0)","(-33.4, 0.0, -37.5)","(-41.0, 0.0, -27.0)","(-51.0, 0.0, -13.0)","(-56.5, 0.0, 0.6)"],["(58.1, 0.0, -0.1)","(45.1, 0.0, -2.0)","(28.8, 0.0, 0.5)","(16.9, 0.0, 1.0)","(-11.5, 0.0, 0.3)","(-17.4, 0.0, -0.2)","(-44.9, 0.0, 2.6)","(-56.5, 0.0, 1.7)"],["(54.0, 0.0, 5.0)","(54.2, 0.0, 18.4)","(43.3, 0.0, 25.7)","(36.5, 0.0, 34.0)","(26.0, 0.0, 45.2)","(0.0, 0.0, 60.8)","(-20.6, 0.0, 51.7)","(-39.9, 0.0, 30.0)","(-52.2, 0.0, 15.2)","(-57.1, 0.0, 2.3)"]],"1":{"state":"in","cfgID":"965","pos":"( -57400, -100, -2310)","fwd":"( 1000, 0, 0)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"2":{"state":"in","cfgID":"965","pos":"( -57460, -100, 2210)","fwd":"( 1000, 0, 0)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"3":{"state":"in","cfgID":"975","pos":"( 57570, -80, -2230)","fwd":"( -1000, 0, 0)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"4":{"state":"in","cfgID":"900","pos":"( -75680, -80, 0)","fwd":"( 0, 0, 1000)","hp":16000,"maxhp":16000,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":660,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"5":{"state":"in","cfgID":"975","pos":"( 57330, -80, 2540)","fwd":"( -1000, 0, 0)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"6":{"state":"in","cfgID":"900","pos":"( 75140, -80, 0)","fwd":"( 0, 0, 1000)","hp":16000,"maxhp":16000,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":660,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"7":{"state":"in","cfgID":"961","pos":"( -62407, -230, -154)","fwd":"( -799, 0, -601)","hp":3750,"maxhp":3750,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":10,"movelock":false,"vis1":false,"vis3":false},"8":{"state":"in","cfgID":"961","pos":"( 62390, -270, 190)","fwd":"( 779, 0, 626)","hp":3750,"maxhp":3750,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":10,"movelock":false,"vis2":false,"vis3":false},"9":{"state":"in","cfgID":"964","pos":"( -17640, -80, -53540)","fwd":"( 929, 0, -370)","hp":1700,"maxhp":1700,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"10":{"state":"in","cfgID":"963","pos":"( -41710, -80, -30310)","fwd":"( 899, 0, -439)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"11":{"state":"in","cfgID":"9622","pos":"( -52300, -80, -17540)","fwd":"( 838, 0, -546)","hp":1300,"maxhp":1300,"mp":0,"maxmp":0,"speed":1500,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"12":{"state":"in","cfgID":"974","pos":"( 17700, -80, -53840)","fwd":"( -707, 0, -707)","hp":1700,"maxhp":1700,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"13":{"state":"in","cfgID":"973","pos":"( 41730, -80, -30320)","fwd":"( -707, 0, -707)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"14":{"state":"in","cfgID":"9722","pos":"( 52390, -80, -17420)","fwd":"( -640, 0, -768)","hp":1300,"maxhp":1300,"mp":0,"maxmp":0,"speed":1500,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"15":{"state":"in","cfgID":"964","pos":"( -17110, -80, 2080)","fwd":"( 1000, 0, 0)","hp":1700,"maxhp":1700,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"16":{"state":"in","cfgID":"963","pos":"( -32930, -80, -2020)","fwd":"( 1000, 0, 0)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"17":{"state":"in","cfgID":"9621","pos":"( -46840, -80, 0)","fwd":"( 1000, 0, 0)","hp":1300,"maxhp":1300,"mp":0,"maxmp":0,"speed":1500,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"18":{"state":"in","cfgID":"974","pos":"( 17110, -80, -2080)","fwd":"( -984, 0, -176)","hp":1700,"maxhp":1700,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"19":{"state":"in","cfgID":"973","pos":"( 32930, -80, 2020)","fwd":"( -971, 0, -240)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"20":{"state":"in","cfgID":"9721","pos":"( 46840, -80, 0)","fwd":"( -871, 0, -491)","hp":1300,"maxhp":1300,"mp":0,"maxmp":0,"speed":1500,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"21":{"state":"in","cfgID":"964","pos":"( -17700, -80, 53840)","fwd":"( 575, 0, 818)","hp":1700,"maxhp":1700,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"22":{"state":"in","cfgID":"963","pos":"( -41550, -80, 30230)","fwd":"( 399, 0, 917)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"23":{"state":"in","cfgID":"9623","pos":"( -52330, -80, 17430)","fwd":"( 70, 0, 998)","hp":1300,"maxhp":1300,"mp":0,"maxmp":0,"speed":1500,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis1":false,"vis3":false},"24":{"state":"in","cfgID":"974","pos":"( 17640, -80, 53540)","fwd":"( -981, 0, 194)","hp":1700,"maxhp":1700,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"25":{"state":"in","cfgID":"973","pos":"( 41710, -80, 30310)","fwd":"( -708, 0, 706)","hp":1500,"maxhp":1500,"mp":0,"maxmp":0,"speed":5000,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"26":{"state":"in","cfgID":"9723","pos":"( 52330, -80, 17430)","fwd":"( -930, 0, 367)","hp":1300,"maxhp":1300,"mp":0,"maxmp":0,"speed":1500,"chrtype":2,"gold":0,"attspeed":0,"att":130,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":0,"movelock":false,"vis2":false,"vis3":false},"27":{"state":"in","cfgID":"101","pos":"( 75000, 0, -1610)","fwd":"( -1000, 0, 0)","hp":800,"maxhp":800,"mp":100,"maxmp":100,"speed":3500,"moving":0,"chrtype":0,"gold":450,"attspeed":0,"att":46,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":15,"movelock":false,"vis2":false,"vis3":false,"Skill0":{"ID":10101,"MaxCD":1000,"canuse":true},"Skill1":{"ID":10110,"MaxCD":4500,"Cost":25,"up":true},"Skill2":{"ID":10120,"MaxCD":10000,"Cost":25,"up":true},"Skill3":{"ID":10130,"MaxCD":45000},"Skill4":{"ID":10002,"MaxCD":90000,"canuse":true},"Skill5":{"ID":64141,"MaxCD":90000,"canuse":true},"Skill6":{"ID":10000,"canuse":true},"Skill7":{"ID":202,"MaxCD":12000,"canuse":true},"Skill8":{"ID":10003,"MaxCD":20000,"canuse":true},"buff":["90010","90025","90020"]},"28":{"state":"in","cfgID":"102","pos":"( -75000, 0, 2060)","fwd":"( 1000, 0, 0)","hp":875,"maxhp":875,"mp":300,"maxmp":300,"speed":6000,"moving":0,"chrtype":0,"gold":210,"attspeed":0,"att":39,"mag":0,"attpen":0,"magpen":0,"attpenrate":0,"magpenrate":0,"Hprec":20,"movelock":false,"vis1":false,"vis3":false,"Skill0":{"ID":10200,"MaxCD":1000,"canuse":true},"Skill1":{"ID":10210,"MaxCD":9000,"Cost":50,"up":true},"Skill2":{"ID":10220,"MaxCD":12000,"Cost":50,"up":true},"Skill3":{"ID":10230,"MaxCD":45000,"Cost":100},"Skill4":{"ID":10002,"MaxCD":90000,"canuse":true},"Skill5":{"ID":64141,"MaxCD":90000,"canuse":true},"Skill6":{"ID":10000,"canuse":true},"Skill7":{"ID":202,"MaxCD":12000,"canuse":true},"Skill8":{"ID":10003,"MaxCD":20000,"canuse":true},"equip0":{"ID":41301.0,"NUM":1.0}}}' http://192.168.142.188:8780
 """
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+#from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 #todo: BaseHTTPServer is for python2, http.server is for python3
-#from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
 from time import gmtime, strftime
 from datetime import datetime
 from random import randint
@@ -34,6 +34,11 @@ from util.stateutil import StateUtil
 class S(BaseHTTPRequestHandler):
     # static variable，目前只支持同一时间处理一场比赛
     prev_stat = None
+    #prev_state_string=None
+    #prev_time=None
+    #
+
+
 
     def __init__(self, *args):
         BaseHTTPRequestHandler.__init__(self, *args)
@@ -46,10 +51,12 @@ class S(BaseHTTPRequestHandler):
     def do_GET(self):
         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
         get_data = self.rfile.read(content_length)  # <--- Gets the data itself
-        #get_data = get_data.decode()
+        get_data = get_data.decode()
         #todo decode for python3 version
-        self.log_file.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " -- " + get_data + "\n")
+        time=gmtime()
+        self.log_file.write(strftime("%Y-%m-%d %H:%M:%S", time) + " -- " + get_data + "\n")
         self.log_file.flush()
+
 
         # 简单解析
         obj = JSON.loads(get_data)
@@ -64,24 +71,40 @@ class S(BaseHTTPRequestHandler):
             print ("clear %s %s" % (S.prev_stat.tick, state_info.tick))
             S.prev_stat = None
         state_info = StateUtil.update_state_log(S.prev_stat, state_info)
-        S.prev_stat = state_info
+
 
         # 构造反馈结果
         # 注：这里帧状态得到了更新，添加了行为信息
-        rsp_str = self.line_trainer.build_heros_response(state_info, self.model)
+
+        #rsp_str = self.line_trainer.build_heros_response(state_info, self.model)
+        #todo：这样更新的state都没有reward，reward是否需要在后续操作中单独更新
+        #下面是修改之后的内容，用作人与机器对战时的训练
+        rsp_str=self.line_trainer.build_PVE_response(state_info,self.model)
         # rsp_str = StateUtil.build_action_response(state_info)
         print(rsp_str)
-
-        # 保存帧状态
-        state_json = JSON.dumps(state_info, cls=ComplexEncoder)
-        self.state_file.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " -- " + state_json + "\n")
-        self.state_file.flush()
-
-        #rsp_str = rsp_str.encode(encoding="utf-8")
+        rsp_str = rsp_str.encode(encoding="utf-8")
         # todo: encode for python3 version
 
         self._set_headers()
         self.wfile.write(rsp_str)
+        #给客户端提供对应的指令
+
+        # 由当前帧状态推测上一帧玩家的action，更新state，无reward，若后面的更新只更新模型reward，可以在分析玩家action时指定reward
+        if S.prev_stat is not None:
+            player_action=Replayer.guess_player_action(S.prev_stat,state_info,"27")
+            #action_str = StateUtil.build_command(player_action)
+            state_info.actions.append(player_action)
+
+        #todo: 根据10帧的双方血量金钱变化，更新state里的reward（仅更新模型控制部分 or 也更新玩家控制部分），并保存state
+        #下面这段还没改
+        state_json = JSON.dumps(state_info, cls=ComplexEncoder)
+        self.state_file.write(strftime("%Y-%m-%d %H:%M:%S", time) + " -- " + state_json + "\n")
+        self.state_file.flush()
+
+        S.prev_stat = state_info
+
+
+
 
     def do_HEAD(self):
         self._set_headers()
--- src/util/linetrainer.py
+++ src/util/linetrainer.py
@@ -17,53 +17,90 @@ class LineTrainer:
 
     # 双方英雄集中到中路中间区域，进行对线
     # 一方英雄回城之后，负责等他满血后回到对战区
-    def build_heros_response(self, state_info, line_model):
-        battle_id = state_info.battleid
-        tick = state_info.tick
+    def build_hero_response(self, state_info, line_model, hero_name):
+        #battle_id = state_info.battleid
+        #tick = state_info.tick
 
         action_strs = []
         for hero in state_info.heros:
-            # 如果有可以升级的技能，直接选择第一个升级
-            skills = StateUtil.get_skills_can_upgrade(hero)
-            if len(skills) > 0:
-                update_str = StateUtil.build_action_command(hero.hero_name, 'UPDATE', {'skillid': str(skills[0])})
-                action_strs.append(update_str)
+            if hero.hero_name==hero_name:
+                break
 
-            if StateUtil.if_hero_at_basement(hero) and hero.hp == hero.maxhp:
-                self.hero_strategy[hero.hero_name] = ActionEnum.line_1
+        # 如果有可以升级的技能，直接选择第一个升级
+        skills = StateUtil.get_skills_can_upgrade(hero)
+        if len(skills) > 0:
+            update_str = StateUtil.build_action_command(hero.hero_name, 'UPDATE', {'skillid': str(skills[0])})
+            action_strs.append(update_str)
+            #todo: 需要把升级append到actions吗？（对于模型会保存action信息到状态帧中）
 
-            # 开始根据策略决定当前的行动
-            # 对线情况下，首先拿到兵线，朝最前方的兵线移动
-            # 如果周围有危险（敌方单位）则启动对线模型
-            near_enemy_heroes = StateUtil.get_nearby_enemy_heros(state_info, hero.hero_name, StateUtil.LINE_MODEL_RADIUS)
-            near_enemy_units = StateUtil.get_nearby_enemy_units(state_info, hero.hero_name, StateUtil.LINE_MODEL_RADIUS)
-            near_enemy_tower = StateUtil.get_nearby_enemy_units(state_info, hero.hero_name, StateUtil.LINE_MODEL_RADIUS)
-            if len(near_enemy_heroes) == 0 and len(near_enemy_units) == 0 and len(near_enemy_tower) == 0:
-                # 跟兵线
-                if hero.hero_name in self.hero_strategy and self.hero_strategy[hero.hero_name] == ActionEnum.line_1:
-                    solider_lines = StateUtil.get_solider_lines(state_info, 1, hero.team)
-                    if len(solider_lines) == 0:
-                        action_str = StateUtil.build_action_command(hero.hero_name, 'HOLD', {})
-                        action_strs.append(action_str)
-                    else:
-                        # 得到最前方的兵线位置
-                        front_point = solider_lines[-1]
-                        print 'front_point, team:%s, line:%s, wave:%s, units:%s' % (front_point.team_id, front_point.line_index, len(solider_lines), len(front_point.units))
-                        move_action = CmdAction(hero.hero_name, CmdActionEnum.MOVE, None, None, front_point.pos, None, None, None, None)
-                        action_str = StateUtil.build_command(move_action)
-                        # action_str = StateUtil.build_action_command(hero.hero_name, 'MOVE', {'pos': '( 5000, -80, 0)'})
-                        action_strs.append(action_str)
-            else:
-                # 使用模型进行决策
-                print 'line model decides'
-                rival_hero = '28' if hero.hero_name == '27' else '27'
-                action = line_model.get_action(state_info, hero.hero_name, rival_hero)
-                action_str = StateUtil.build_command(action)
-                action_strs.append(action_str)
+        if StateUtil.if_hero_at_basement(hero) and hero.hp == hero.maxhp:
+            self.hero_strategy[hero.hero_name] = ActionEnum.line_1
 
-                # 保存action信息到状态帧中
-                state_info.actions.append(action)
+        # 开始根据策略决定当前的行动
+        # 对线情况下，首先拿到兵线，朝最前方的兵线移动
+        # 如果周围有危险（敌方单位）则启动对线模型
+        near_enemy_heroes = StateUtil.get_nearby_enemy_heros(state_info, hero.hero_name, StateUtil.LINE_MODEL_RADIUS)
+        near_enemy_units = StateUtil.get_nearby_enemy_units(state_info, hero.hero_name, StateUtil.LINE_MODEL_RADIUS)
+        near_enemy_tower = StateUtil.get_nearby_enemy_units(state_info, hero.hero_name, StateUtil.LINE_MODEL_RADIUS)
+        if len(near_enemy_heroes) == 0 and len(near_enemy_units) == 0 and len(near_enemy_tower) == 0:
+            # 跟兵线
+            if hero.hero_name in self.hero_strategy and self.hero_strategy[hero.hero_name] == ActionEnum.line_1:
+                solider_lines = StateUtil.get_solider_lines(state_info, 1, hero.team)
+                if len(solider_lines) == 0:
+                    action_str = StateUtil.build_action_command(hero.hero_name, 'HOLD', {})
+                    #todo: 需不需要reward
+                    action_strs.append(action_str)
+                    #todo:这边需要给state.actions append 对应的 action 不？
+                else:
+                    # 得到最前方的兵线位置
+                    front_point = solider_lines[-1]
+                    print('front_point, team:%s, line:%s, wave:%s, units:%s' % (front_point.team_id, front_point.line_index, len(solider_lines), len(front_point.units)))
+                    move_action = CmdAction(hero.hero_name, CmdActionEnum.MOVE, None, None, front_point.pos, None, None, None, None)
+                    #todo:需不需要reward
+                    action_str = StateUtil.build_command(move_action)
+                    # action_str = StateUtil.build_action_command(hero.hero_name, 'MOVE', {'pos': '( 5000, -80, 0)'})
+                    action_strs.append(action_str)
+                    #todo:这里也是actions.append(action)的问题
+        else:
+            # 使用模型进行决策
+            print('line model decides')
+            rival_hero = '28' if hero.hero_name == '27' else '27'
+            action = line_model.get_action(state_info, hero.hero_name, rival_hero)
+            #todo：这里没有reward
+            action_str = StateUtil.build_command(action)
+            action_strs.append(action_str)
 
-        rsp_obj = {"ID": battle_id, "tick": tick, "cmd": action_strs}
-        rsp_str = JSON.dumps(rsp_obj)
+            # 保存action信息到状态帧中
+            state_info.actions.append(action)
+
+        #rsp_obj = {"ID": battle_id, "tick": tick, "cmd": action_strs}
+        #rsp_str = JSON.dumps(rsp_obj)
+        #return rsp_str
+        return action_strs
+
+    def build_EVE_response(self,state_info, line_model):
+        #完成之前的 build_heros_response() 的功能
+        battle_id = state_info.battleid
+        tick = state_info.tick
+        action_strs=[]
+        for hero in state_info.heros:
+            action=self.build_hero_response(state_info,line_model,hero.hero_name)
+            action_strs=action_strs+action
+        rsp_obj={"ID": battle_id, "tick":tick, "cmd":action_strs}
+        rsp_str=JSON.dump(rsp_obj)
         return rsp_str
+
+
+
+    def build_PVE_response(self,state_info, line_model):
+        # 仅生成敌方英雄的action，不对己方英雄做任何指令
+        battle_id = state_info.battleid
+        tick = state_info.tick
+        action_strs = []
+
+        action = self.build_hero_response(state_info, line_model, "28")
+        action_strs = action_strs + action
+        rsp_obj = {"ID": battle_id, "tick": tick, "cmd": action_strs}
+        rsp_str = JSON.dump(rsp_obj)
+        return rsp_str
+
