20e06f590f3546f77a48bac4792f992b37d00bbd
Xin Yu <scaner@gmail.com>
2018-01-12 23:14:33 +0800

    修正gateway的逻辑,决策代理工作. #27

--- src/gateway.py
+++ src/gateway.py
@@ -13,9 +13,6 @@ from common import cf as C
 
 #define("port", default=8780, help="run on the given port", type=int)
 
-"""
-b = b'{"wldstatic":{"ID":21},"wldruntime":{"tick":29502.0},"27":{"Skill0":{"ID":10101,"MaxCD":1000,"canuse":true,"CD":0},"Skill1":{"ID":10110,"MaxCD":4500,"Cost":25,"up":false,"canuse":true,"CD":0},"Skill2":{"ID":10120,"MaxCD":10000,"Cost":25,"up":false,"canuse":false,"CD":0},"Skill3":{"ID":10130,"MaxCD":45000,"up":false,"canuse":false,"CD":0},"Skill4":{"ID":10002,"MaxCD":90000,"canuse":true},"Skill5":{"ID":64141,"MaxCD":90000,"canuse":true},"Skill6":{"ID":10000,"canuse":true},"Skill7":{"ID":202,"MaxCD":12000,"canuse":true},"Skill8":{"ID":10003,"MaxCD":20000,"canuse":true},"buff":["369"]},"28":{"Skill0":{"ID":10101,"MaxCD":1000,"canuse":true,"CD":0},"Skill1":{"ID":10110,"MaxCD":4500,"Cost":25,"up":false,"canuse":true,"CD":0},"Skill2":{"ID":10120,"MaxCD":10000,"Cost":25,"up":false,"canuse":false,"CD":0},"Skill3":{"ID":10130,"MaxCD":45000,"canuse":false,"CD":0,"up":false},"Skill4":{"ID":10002,"MaxCD":90000,"canuse":true},"Skill5":{"ID":64141,"MaxCD":90000,"canuse":true},"Skill6":{"ID":10000,"canuse":true},"Skill7":{"ID":202,"MaxCD":12000,"canuse":true},"Skill8":{"ID":10003,"MaxCD":20000,"canuse":true}},"29":{"pos":"( 44277, 0, -1843)","fwd":"( -989, 0, 147)"},"30":{"pos":"( -42454, 0, 1528)"},"31":{"pos":"( 53658, 0, -14946)","fwd":"( -192, 0, -981)"},"32":{"pos":"( -51154, 0, -13229)"},"33":{"pos":"( 54511, 0, 14685)","fwd":"( -82, 0, 997)"},"34":{"pos":"( -54100, 0, 16015)"},"35":{"pos":"( 47301, 0, -1660)"},"36":{"pos":"( -45494, 0, 1848)","fwd":"( 994, 0, -105)"},"37":{"pos":"( 54242, 0, -11946)"},"38":{"pos":"( -52735, 0, -10628)","fwd":"( 358, 0, -934)"},"39":{"pos":"( 54759, 0, 11645)","fwd":"( -81, 0, 997)"},"40":{"pos":"( -54857, 0, 13055)","fwd":"( 248, 0, 969)"},"41":{"pos":"( 50325, 0, -1185)"},"42":{"pos":"( -48490, 0, 1474)"},"43":{"pos":"( 54829, 0, -8949)","fwd":"( -192, 0, -981)"},"44":{"pos":"( -53835, 0, -7780)","fwd":"( 359, 0, -933)"},"45":{"pos":"( 55007, 0, 8605)"},"46":{"pos":"( -55617, 0, 10095)"},"47":{"pos":"( 53349, 0, -705)"},"48":{"pos":"( -51466, 0, 798)","fwd":"( 975, 0, 222)"},"49":{"pos":"( 55406, 0, -5946)"},"50":{"pos":"( -54939, 0, -4932)"},"51":{"pos":"( 55247, 0, 5565)"},"52":{"pos":"( -56377, 0, 7135)"}}'
-"""
 GAME_BASE_PORT = C.GAME_BASE_PORT
 GAME_WORKER_SLOTS = C.GAME_WORKER_SLOTS
 
@@ -72,9 +69,9 @@ class ForwardHandler(tornado.web.RequestHandler):
             info['id'] = battle_id
             info['port'] = port
 
-            uri = 'http://localhost:%d/' % port
+            uri = 'http://127.0.0.1:%d/' % port
             fetch_request(
-                self.request.uri, handle_response,
+                uri, handle_response,
                 method=self.request.method, body=body,
                 headers=self.request.headers, follow_redirects=False,
                 allow_nonstandard_methods=True)
