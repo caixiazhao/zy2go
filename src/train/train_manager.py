e2518580aae4548e27bb078e980c914147002bfc
Xin Yu <scaner@gmail.com>
2018-01-13 21:04:08 +0800

    train_manager: 每个版本都保存一份数据到磁盘上.

--- src/train/train_manager.py
+++ src/train/train_manager.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import shutil
 import pickle
+
 
 from util.modelprocess import ModelProcess
 from common import cf as C
@@ -19,6 +22,9 @@ class LineTrainerManager:
 
     def push_data(self, data):
         o4r = pickle.loads(data)
+        #TODO temperay ignore upload data generation.
+        o4r['generation_id'] = self.generation_id
+
         if o4r['generation_id'] != self.generation_id:
             print("/data %d %d %d skip" % (
                 o4r['battle_id'], o4r['generation_id'],
@@ -29,12 +35,19 @@ class LineTrainerManager:
                 o4r['battle_id'], o4r['generation_id'],
                 len(data), len(self.train_data), C.TRAIN_GAME_BATCH))
 
-
     def train(self):
         train_data = list(self.train_data)
         self.train_data.clear()
         self.model_process.do_real_train(train_data)
-        # self.generation_id += 1
+        self.generation_id += 1
+        self.dump_model_to_disk()
+
+    def dump_model_to_disk(self):
+        base_path = os.path.join(C.DATA_ROOT_PATH, "trainer", str(self.get_generation_id()))
+        shutil.rmtree(base_path)
+        os.makedirs(base_path)
+        self.model_process.model_1.save(base_path + '/1')
+        self.model_process.model_2.save(base_path + '/2')
 
     def start(self):
         print('start')
