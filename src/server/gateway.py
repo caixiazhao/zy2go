f4fc1b533d3fdec4b117621f69450dd60e56962e
858675898 <31403118+858675898@users.noreply.github.com>
2018-01-30 12:30:45 +0800

    添加模型变量转发，和心跳请求，同步generation

--- src/server/gateway.py
+++ src/server/gateway.py
@@ -1,6 +1,7 @@
+import pickle
 import sys
 import hashlib
-
+import time
 import tornado.httpserver
 import tornado.httpclient
 import tornado.httputil
@@ -32,6 +33,13 @@ def fetch_request(url, callback, **kwargs):
     client = tornado.httpclient.AsyncHTTPClient()
     client.fetch(req, callback, raise_error=False)
 
+#据时效性，和对返回值的需求程度，设置不同的timeout时间
+def fetch_request1(url, callback, **kwargs):
+    req = tornado.httpclient.HTTPRequest(url, request_timeout=200, **kwargs)
+    client = tornado.httpclient.AsyncHTTPClient()
+    client.fetch(req, callback, raise_error=False)
+
+
 
 class ForwardHandler(tornado.web.RequestHandler):
     def __init__(self, application, request, **kwargs):
@@ -43,6 +51,21 @@ class ForwardHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
         info = {}
+
+        def train__id_callback(response):
+            if not response.body:
+                print(response)
+            else:
+                C.set_generation_id(int(response.body))
+        C.END = time.time()
+        #定时获取generation_id，确保id一致
+        if C.END - C.START >10:
+            fetch_request(
+                'http://127.0.0.1:%d/generation_id' %C.TRAINER_PORT,
+                train__id_callback,
+                method='GET', follow_redirects=False,
+                allow_nonstandard_methods=True)
+            C.START = time.time()
 
         if self.request.path == '/generation_id':
             self.finish(str(C.get_generation_id()))
@@ -89,15 +112,45 @@ class DataHandler(tornado.web.RequestHandler):
     def get(self):
         body = self.request.body
         path = self.request.path
+
+        def train__model_callback(response):
+            if (response.error and not isinstance(response.error, tornado.httpclient.HTTPError)):
+                self.set_status(500)
+                self.write('Internal server error:\n' + str(response.error))
+            else:
+                if response.body:
+                    self.write(response.body)
+            self.finish()
+
+
+        if path== '/data/model':
+            fetch_request1(
+                'http://127.0.0.1:%d%s' % (C.TRAINER_PORT,  path[5:]),
+                train__model_callback,
+                method='GET', body=body,
+                follow_redirects=False,
+                allow_nonstandard_methods=True)
+            return
+
+
         generation_id, battle_id, model_name = path[6:].split('/')
         print(battle_id, model_name, generation_id)
 
+        # self.finish(str(C.get_generation_id()+","+C.get_server_id()))
         self.finish(str(C.get_generation_id()))
 
         def train__data_callback(response):
-            cur_generation_id = int(response.body)
+            if not response.body:
+                batches =  G['batches']
+                cur_generation_id = int(generation_id)
+                print(response)
+            else:
+                cur_generation_id = int(bytes.decode(response.body).split(",")[0])
+                batches = int(bytes.decode(response.body).split(",")[1])
+
             if cur_generation_id == int(generation_id):
-                G['batches'] += 1
+                G['batches'] = batches
+                print("the train_dict len",batches)
 
             if cur_generation_id != C.get_generation_id():
                 G['batches'] = 0
@@ -115,12 +168,15 @@ class DataHandler(tornado.web.RequestHandler):
         def train__train_callback(response):
             G['train_lock'] = False
             G['batches'] = 0
-            C.set_generation_id(int(response.body))
+            if not response.body:
+                print(response)
+            else:
+                C.set_generation_id(int(response.body))
 
         if G['train_lock']:
             return
 
-        fetch_request(
+        fetch_request1(
             'http://127.0.0.1:%d%s' % (C.TRAINER_PORT, self.request.path),
             train__data_callback,
             method='GET', body=body,
@@ -146,9 +202,8 @@ def run_gateway(port, start_ioloop=True):
 
 if __name__ == '__main__':
     port = 8780
-    if len(sys.argv) > 1:
-        port = int(sys.argv[1])
-
+    if len(sys.argv) > 2:
+        port = int(sys.argv[2])
     print("Starting HTTP proxy on port %d" % port)
     C.set_run_mode("gateway")
     C.set_generation_id(0)
